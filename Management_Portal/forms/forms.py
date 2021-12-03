7978927c55cfd079d3eff35fd4d602cf31de685a
Mary Kilewe <marykilewe@gmail.com>
2021-12-03 23:58:00 +0300

    updated form labels

--- Management_Portal/forms/forms.py
+++ Management_Portal/forms/forms.py
@@ -8,13 +8,13 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo, NumberRange
 class PatientRegForm(FlaskForm):
     ''' Facilitates addition of newly registered patients '''
 
-    first_name = StringField('*First Name', validators=[DataRequired(), Regexp('^\w+$', message="Username must contain only letters numbers or underscore"), Length(min=2, max=30)])
-    last_name = StringField('*Last Name', validators=[DataRequired(), Length(min=2, max=30)])
+    first_name = StringField('First Name', validators=[DataRequired(), Regexp('^\w+$', message="Username must contain only letters numbers or underscore"), Length(min=2, max=30)])
+    last_name = StringField('Last Name', validators=[DataRequired(), Length(min=2, max=30)])
     gender = SelectField('Gender', choices=[ ('Female', 'Female'), ('Male', 'Male')], validators=[DataRequired()])
-    age = IntegerField('Age', validators=[NumberRange(min=0, max=150)])
-    facility = StringField('*Health Facility', validators=[DataRequired(), Length(min=2, max=100)])
+    age = IntegerField('Age', validators=[DataRequired(), NumberRange(min=0, max=150)])
+    facility = StringField('Health Facility', validators=[DataRequired(), Length(min=2, max=60)])
     county = SelectField('County', choices=[])
-    reg_date = DateField('*Registration Date', validators=[DataRequired()])
+    reg_date = DateField('Registration Date', validators=[DataRequired()])
 
     submit = SubmitField('Add')
 
